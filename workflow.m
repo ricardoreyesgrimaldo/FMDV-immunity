423ccb2a0f44def04d856c96eff4272ab8fc9ce9
ricardoreyesgrimaldo <46801817+ricardoreyesgrimaldo@users.noreply.github.com>
2019-05-20 20:08:54 -0700

    Update workflow.m
    Bugs fixed, still need to double check the likelihood ration test and include the AIC for the different models.

--- workflow.m
+++ workflow.m
@@ -196,7 +196,7 @@ end
 loglrt=maxlikelihoodmodel1-maxlikelihoodmodel2;
 loglikelihoodtest=chi2cdf(2*loglrt,1);%<-LRT
 simdistribution(optimodel1,optimodel2,highmodel1,countmodel1,lowmodel1,count_lowmodel1,highmodel2,countmodel2,lowmodel2,count_lowmodel2,high1,count1,low1,count_low1)
-save('run6.mat')
-Likelihoodtest
+save('run7.mat')
+%Likelihoodtest
 toc
 sprintf('workflow done')
